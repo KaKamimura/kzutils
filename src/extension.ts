a0bd028650059bfa687152051a8d8edd501fcab0
KaKamimura <KaKamimura@mx1.wiseman.co.jp>
2020-09-04 10:50:11 +0900

    .

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('kzutils.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('kzutils.alignmentbystring', () => {
 		if (!vscode.window.activeTextEditor
 			|| !vscode.window.activeTextEditor.document
 			|| vscode.window.activeTextEditor.selection.isEmpty) {
